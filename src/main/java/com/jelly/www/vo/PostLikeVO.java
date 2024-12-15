862e5c71f9b38d6e7beced538475003850757b95
Justin <justinlee13304@gmail.com>
2024-12-15 17:42:52 +0900

    Merge branch 'feature/style' of https://github.com/HyeongYoon/Haribo.git into feature/style

--- src/main/java/com/jelly/www/vo/PostLikeVO.java
+++ src/main/java/com/jelly/www/vo/PostLikeVO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostLikeVO {
-	private int like_id; // 좋아요 고유 ID
+    private int like_id; // 좋아요 고유 ID
     private int post_id; // 게시물 ID (POST 테이블 참조)
     private int user_id; // 사용자 ID (USER 테이블 참조)
     private Timestamp liked_at; //좋아요를 누른 시간
